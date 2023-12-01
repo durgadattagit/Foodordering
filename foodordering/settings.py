73a35f3b9b79bfa03178827587749c41986ec93d
durgadattam21@gmail.com <durgadattam21@gmail.com>
2023-12-01 20:36:41 +0530

    Added product app in setting.py

--- foodordering/settings.py
+++ foodordering/settings.py
@@ -37,7 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
 ]
+
+External_apps =[
+    'orderapp',
+]
+INSTALLED_APPS += External_apps
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
